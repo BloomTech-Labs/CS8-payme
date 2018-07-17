1405b3f714256049300fba5a6dca8cf2f4a3b6df
Maximo DeLaRosa <mjd809@gmail.com>
2018-07-17 15:57:09 -0400

    Commit before merge

--- server/app.js
+++ server/app.js
@@ -4,22 +4,19 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
-const app = express();
 
 const routes = require('./routes/');
 const keys = require('./config/keys');
 const port = process.env.PORT || 5000;
+const app = express();
 
 // Connecting to mLab/port
 mongoose
   .connect(process.env.DATA_BASE || keys.mongoURI,  { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to DB');
-  })
-  .catch(err => {
-    console.log('Error connecting to DB', err);
-  });
-app.listen(port, err => {
+  .then(() => console.log('Connected to DB'))
+  .catch(err => console.log('Error connecting to DB', err));
+
+  app.listen(port, () => {
     console.log(`connected to the server port ${port}`);
 });
 
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
   secret: process.env.SECRET,
+  mongoURI: process.env.mongoURI
 };
