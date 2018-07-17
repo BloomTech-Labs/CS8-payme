bf1e1537507298a6e2d8604bbe1e3eb12c8bfd61
Maximo DeLaRosa <mjd809@gmail.com>
2018-07-17 00:29:52 -0400

    Add Invoice functionality
    Add invoice is still buggy

--- client/src/components/invoices/addInvoice.js
+++ client/src/components/invoices/addInvoice.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
-
+import Sidebar from '../sidebar/';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
 class AddInvoice extends Component {
   state = {}
-  render() { 
+  handleFormSubmit = ({ username, password }) => {
+    this.props.login(username, password, this.props.history);
+  };
+
+  render() {
+    const { handleSubmit } = this.props;  
     return ( 
       <div>
-        <h1>Hey</h1>
+        <Sidebar />
+        <h1>HII</h1>
+        {/* <form className="signin--signin" onSubmit={this.handleFormSubmit}>
+            <Field
+              name="username"
+              component="input"
+              className="signin--signin__username"
+              placeholder="Username"
+            />
+            <br />
+            <Field
+              name="password"
+              component="input"
+              className="signin--signin__password"
+              placeholder="Password"
+            />
+            <br />
+            <button
+              className="signin--signin__button"
+              action="submit"
+              value="Sign In"
+            >Sign In</button>
+          </form> */}
       </div>
      )
   }
 }
  
-export default AddInvoice;
+AddInvoice = connect(null, )(AddInvoice);
+
+export default reduxForm({
+  form: 'logginIn', // Unique name for the form
+  fields: ['username', 'password'],
+})(AddInvoice);
--- client/src/components/invoices/index.js
+++ client/src/components/invoices/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import Sidebar from '../sidebar/';
 
 class Invoices extends Component {
@@ -8,14 +9,15 @@ class Invoices extends Component {
         <div className="invoice">
           <Sidebar />
           <div className="invoice-box">
-            <p>Id of invoice</p>
+            <p className="invoice-id">#23242342</p>
             <p>Name</p>
             <p>Company Inc</p>
             <p>Email@gmail.com</p>
-            <p>phone number <span> Invoice PDF</p>
+            <p>phone number <span className="invoice-pdf"> Invoice PDF</span></p>
             <hr />
             <p>Weekly</p>
           </div>
+          <NavLink to="/addinvoice"><p className="invoice-add">Add Invoice</p></NavLink>
         </div>
      )
   }
