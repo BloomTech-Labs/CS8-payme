d3c21b57aa791afebba69d58bd51700cc56c9f64
Maximo DeLaRosa <mjd809@gmail.com>
2018-07-17 13:53:36 -0400

    **test commit

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,6 @@
 
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer } from 'redux-form';
 
 import {
   AUTHENTICATION_ERROR,
