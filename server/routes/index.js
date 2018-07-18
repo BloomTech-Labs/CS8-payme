2a7e3b42c7da1f7092f7ae52d829dc2146655dba
AJ Genung <DragonRydrz@me.com>
2018-07-17 20:57:07 -0400

    added /api/invoices routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,20 +2,22 @@ const { register } = require('../controllers/user/registerUser');
 const { login, checkToken } = require('../controllers/user/loginUser');
 const { changePassword } = require('../controllers/user/changePassword');
 const { addInvoice } = require('../controllers/invoice/newInvoice');
+const {
+  getAllInvoices,
+  getOneInvoice,
+} = require('../controllers/invoice/getInvoice');
 
 const { restricted, authenticate } = require('../config/auth');
 
 const { createReminder } = require('../controllers/sendText');
 
 module.exports = app => {
-  // app.get('/', (req, res) => {
-  //   res.json({ message: 'API running successfully! YAY!' });
-  // });
-
   app.post('/api/register', register);
   app.post('/api/login', authenticate, login);
   app.post('/api/sms', restricted, createReminder);
   app.get('/api/login', restricted, checkToken);
   app.post('/api/changepassword', restricted, changePassword);
   app.post('/api/addinvoice', restricted, addInvoice);
+  app.get('/api/invoices', restricted, getAllInvoices);
+  app.get('/api/invoices/:number', restricted, getOneInvoice);
 };
