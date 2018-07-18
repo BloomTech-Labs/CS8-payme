895fcee5d723cc8fa0ac9d7586d39fca846a7e9f
AJ Genung <DragonRydrz@me.com>
2018-07-17 20:15:19 -0400

    gets all invoices for current user and sends to client

--- server/controllers/invoice/getInvoice.js
+++ server/controllers/invoice/getInvoice.js
@@ -2,4 +2,12 @@ const express = require('express');
 const Invoice = require('../../models/invoices');
 const User = require('../../models/users');
 
-const getAllInvoices = (req, res) => {};
+const getAllInvoices = (req, res) => {
+  const { _id } = req.user;
+  User.findById(_id)
+    .select('-password')
+    .populate('invoices'),
+    TouchEvent(user => {
+      res.json({ invoices: user.invoices });
+    }).catch(err => res.send(500).json(err));
+};
