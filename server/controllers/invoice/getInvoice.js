9c64ccc07e00e54eb6dfb84499314606266ead12
AJ Genung <DragonRydrz@me.com>
2018-07-17 20:56:32 -0400

    added ability to get one invoice instead of all

--- server/controllers/invoice/getInvoice.js
+++ server/controllers/invoice/getInvoice.js
@@ -3,11 +3,30 @@ const Invoice = require('../../models/invoices');
 const User = require('../../models/users');
 
 const getAllInvoices = (req, res) => {
-  const { _id } = req.user;
-  User.findById(_id)
-    .select('-password')
-    .populate('invoices'),
-    TouchEvent(user => {
-      res.json({ invoices: user.invoices });
-    }).catch(err => res.send(500).json(err));
+  const { invoices } = req.user;
+  res.json(invoices);
+  // User.findById(_id)
+  //   .select('-password')
+  //   .populate('invoices')
+  //   .then(user => {
+  //     res.json({ invoices: user.invoices });
+  //   })
+  //   .catch(err => res.send(500).json(err));
+};
+
+const getOneInvoice = (req, res) => {
+  const { invoices } = req.user;
+  const { number } = req.params;
+  console.log(req.params);
+  for (invoice of invoices) {
+    if (invoice.number === number) {
+      return res.json(invoice);
+    }
+  }
+  res.status(404).json({ message: 'Invoice number does not exist.' });
+};
+
+module.exports = {
+  getOneInvoice,
+  getAllInvoices,
 };
