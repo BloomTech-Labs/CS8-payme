c79973ad5c4170a3a6146b0597b879dbe0900b75
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-07-19 19:20:44 -0400

    email working

--- server/controllers/sendEmail.js
+++ server/controllers/sendEmail.js
@@ -1,60 +1,68 @@
-// Author John C.
-
-require('dotenv').load();
+require("dotenv").load();
 const nodemailer = require("nodemailer");
 
 const Invoice = require("../models/invoices");
-const User = require("../models/users");
 
 // Generate test SMTP service account from ethereal.email
 // Only needed if you don't have a real mail account for testing
 
 const sendEmail = (req, res) => {
-	// pass in message to send, invoice ID, and user ID into post request
+	// pass in message to send, invoice ID into post request
 	const {
-		message
-		// invoiceID, // uncomment these once you have appropriate IDs from database
+		message,
+		invoiceID // uncomment these once you have appropriate IDs from database
 		// userID
 	} = req.body;
 
-	// ====== TODO =======
-	// const invoice = Invoice.find({ id: invoiceID }); find the user by id passed in on post
-	// const user = User.find({ id: userID }); find the user by id passed in on post
-	// when these are found, pass into mailoptions in appropriate fields
+	if (!message || !invoiceID) {
+		return res
+			.status(422)
+			.json({ message: "Message and Invoice ID required." });
+	}
 
-	nodemailer.createTestAccount((err, account) => {
-		// create reusable transporter object using the default SMTP transport
-		const transporter = nodemailer.createTransport({
-			host: "smtp.ethereal.email",
-			port: 587,
-			auth: {
-				user: "nzopjkf67k7li4wv@ethereal.email",
-				pass: "VFsAdsD4CRdU2NJ2wC"
-			}
-		});
+	Invoice.findById(invoiceID) // find the user by id passed in on post
+		.then(invoice => {
+			// searches the database of invoices by a specific ID
+			console.log(invoice);
+			if (!invoice) return res.status(404).json({ error: "Invoice not found" });
+			nodemailer.createTestAccount((err, account) => {
+				// create reusable transporter object using the default SMTP transport
+				const transporter = nodemailer.createTransport({
+					host: "smtp.ethereal.email",
+					port: 587,
+					auth: {
+						user: "nzopjkf67k7li4wv@ethereal.email",
+						pass: "VFsAdsD4CRdU2NJ2wC"
+					}
+				});
 
-		// setup email data with unicode symbols
-		let mailOptions = {
-			from: '"GiveMeMyMoney" <Now@givememymoney.com>', // sender address
-			to: "bar@example.com, baz@example.com", // list of receivers
-			subject: "Hello ✔", // Subject line
-			text: message, // plain text body
-			html: `<b>${message}</b>` // html body
-		};
+				if (invoice.email.address === undefined)
+					res.json({ error: "No email found on Invoice." });
+				// setup email data with unicode symbols
+				let mailOptions = {
+					from: '"GiveMeMyMoney" <Now@givememymoney.com>', // sender address
+					to: `${invoice.email.address}`, // list of receivers
+					subject: "Hello ✔", // Subject line
+					text: `${message}`, // plain text body
+					html: `<b>${message} <br /> ${JSON.stringify(invoice)}</b>` // html body
+				};
 
-		// send mail with defined transport object
-		transporter.sendMail(mailOptions, (error, info) => {
-			if (error) {
-				res.json({ error: "Email not sent" });
-				return console.log(error);
-			}
-			res.send({
-				success: "Email sent",
-				messageID: info.messageId,
-				messageURL: nodemailer.getTestMessageUrl(info)
+				// send mail with defined transport object
+				transporter.sendMail(mailOptions, (error, info) => {
+					if (error) {
+						return res.json({ error: "Email not sent", body: error });
+					}
+					res.send({
+						success: "Email sent",
+						messageInfo: info,
+						messageURL: nodemailer.getTestMessageUrl(info)
+					});
+				});
 			});
+		})
+		.catch(error => {
+			res.status(501).json(error);
 		});
-	});
 };
 
 module.exports = {
