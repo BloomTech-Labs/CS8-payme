c195c269aafc84332c2afeac36bdab671c874c72
unknown <luisd824@gmail.com>
2018-07-19 14:54:02 -0400

    scheduler working

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,20 +4,26 @@ const { changePassword } = require('../controllers/user/changePassword');
 const { addInvoice } = require('../controllers/invoice/newInvoice');
 const {
   getAllInvoices,
-  getOneInvoice,
+  getOneInvoice
 } = require('../controllers/invoice/getInvoice');
 
 const { restricted, authenticate } = require('../config/auth');
 
-const { createReminder } = require('../controllers/sendText');
+const {
+  createReminder,
+  getReminder,
+  deleteReminder
+} = require('../controllers/sendText');
 
 module.exports = app => {
   app.post('/api/register', register);
   app.post('/api/login', authenticate, login);
-  app.post('/api/sms', restricted, createReminder);
   app.get('/api/login', restricted, checkToken);
   app.post('/api/changepassword', restricted, changePassword);
   app.post('/api/addinvoice', restricted, addInvoice);
   app.get('/api/invoices', restricted, getAllInvoices);
   app.get('/api/invoices/:number', restricted, getOneInvoice);
+  app.post('/api/sms', createReminder);
+  app.get('/api/sms/:id', getReminder);
+  app.post('/api/sms/:id', deleteReminder);
 };
