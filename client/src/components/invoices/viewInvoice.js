d6729ec00c398a0282d372399ac3aec3c1ed5042
Maximo DeLaRosa <mjd809@gmail.com>
2018-07-22 10:51:17 -0400

    Added css loader for loading data

--- client/src/components/invoices/viewInvoice.js
+++ client/src/components/invoices/viewInvoice.js
@@ -6,6 +6,7 @@ import { handleInvoiceIdx, getAllInvoices, getInvoice } from '../../actions/invo
 import Sidebar from '../sidebar';
 import DeleteInvoice from './deleteInvoice';
 import UpdateInvoice from './updateInvoice';
+import ViewExport from './viewexport';;
 
 class ViewInvoice extends Component {
   state = {
@@ -55,37 +56,10 @@ class ViewInvoice extends Component {
           <Link to="/invoices"><p><i className="fas fa-arrow-left fa-fw" /></p></Link>
           {invoice ? (
             <div>
-            {!this.state.updateField ? 
-              (
-                <div className="view-invoice-box">
-                  <div className="view-invoice-details">
-                  <div className="view-invoice-details-flex">
-                      <p>Invoice pdf(not working)</p>
-                      <p>{invoice.pdf}</p>
-                    </div>
-                    <div className="view-invoice-details-flex">
-                      <p>Invoice Number:</p>
-                      <p>{invoice.number}</p>
-                    </div>
-                    <div className="view-invoice-details-flex">
-                      <p>Client Name:</p>
-                      <p>{invoice.clientName}</p>
-                    </div>
-                    <div className="view-invoice-details-flex">
-                      <p>Company Name</p>
-                      <p>{invoice.companyName}</p>
-                    </div>
-                    <div className="view-invoice-details-flex">
-                      <p>Total Amount: $</p>
-                      <p className="view-invoice-details-total">{invoice.totalAmount}</p>
-                    </div>
-                  </div>
-                </div>
-              )
-              : <UpdateInvoice />
-
-            }
-            {this.state.modalTrigger ? (
+              {!this.state.updateField ? (
+                <ViewExport invoice={invoice} />
+              ) : <UpdateInvoice /> }
+              {this.state.modalTrigger ? (
                 <div>
                   <DeleteInvoice
                     toggleModal={this.toggleModal}
@@ -94,10 +68,9 @@ class ViewInvoice extends Component {
                     history={this.props.history}
                   />
                 </div>
-            ) : null}
-          </div>
-          ) : null }
-          
+              ) : null}
+            </div>
+          ) : <div className="loader">Loading..</div> }
         </div>
       </div>
     );
