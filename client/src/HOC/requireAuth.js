076c115879d4b3e97348bcb40e96392977a9febb
AJ Genung <DragonRydrz@me.com>
2018-07-24 12:14:22 -0400

    heroku push, updated HOC will Mount

--- client/src/HOC/requireAuth.js
+++ client/src/HOC/requireAuth.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default ComposedComponent => {
   class RequireAuthentication extends Component {
     componentWillMount() {
-      if (!localStorage.getItem('id')) this.props.history.push('/signin');
+      // if (!localStorage.getItem('id')) this.props.history.push('/signin');
+      // console.log(axios);
+      axios
+        .get('/api/login', { headers: { Authorization: `bearer ${localStorage.getItem('id')}` } })
+        .then(res => {
+          localStorage.setItem('id', res.data.token);
+        })
+        .catch(err => {
+          this.props.history.push('/signin');
+        });
     }
 
     render() {
-      return (
-        <div>
-          {localStorage.getItem('id') && (
-            <ComposedComponent {...this.props} />
-          )}
-        </div>
-      );
+      // console.log(axios);
+      return <ComposedComponent {...this.props} />;
     }
   }
 
-  return (RequireAuthentication);
+  return RequireAuthentication;
 };
