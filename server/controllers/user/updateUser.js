d0bb949f764787d02ac1519931e396b04882524f
AJ Genung <DragonRydrz@me.com>
2018-07-25 17:13:16 -0400

    stripe info must be sent to route

--- server/controllers/user/updateUser.js
+++ server/controllers/user/updateUser.js
@@ -4,6 +4,10 @@ const updateStripeInfo = (req, res) => {
   const { stripe } = req.body;
   const { _id } = req.user;
 
+  if (!stripe || !stripe.code || !stripe.scope) {
+    return res.status(422).json({ message: 'Stripe info required.' });
+  }
+
   User.findByIdAndUpdate(_id, { stripe }, { new: true })
     .select('-password')
     .populate('invoices')
