52f3dff70f12b1c9e8ad1969613b9d21d1a4298e
Maximo DeLaRosa <mjd809@gmail.com>
2018-07-27 22:58:05 -0400

    finished signin page

--- client/src/components/Auth/signin.js
+++ client/src/components/Auth/signin.js
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom';
 import { Field, reduxForm } from 'redux-form';
 import { login } from '../../actions/auth';
 
-// import logo from './google.png';
+import signin from './signin.jpg';
 import backgroundImage from './background.jpg';
 
-const styles = {
-  backgroundImage: `url(${backgroundImage})`,
-};
+// const styles = {
+//   backgroundImage: `url(${backgroundImage})`,
+//   backgroundSize: 'cover',
+// };
 
 class Signin extends Component {
   handleFormSubmit = ele => {
@@ -19,57 +20,60 @@ class Signin extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
-      <div className="signin" style={styles}>
-        <p className="signin-headline">
+      <div className="signin">
+        <div className="signin-foreground">
+        {/* <p className="signin-headline">
           Lorem ipsum <br /> <span className="signup-headline2">dolor sit amet</span>
-        </p>
-        <div className="signin--box">
-          <h1 className="signin--box--title">
-            payMe
-            <span className="signin--box--dot">
-            .
-            </span>
-            <br />
-          </h1>
-          <h1 className="signin--header">Sign In</h1>
-          <h3>{this.props.message}</h3>
-          <form className="signin--form" onSubmit={handleSubmit(this.handleFormSubmit)}>
-            <Field
-              name="username"
-              component="input"
-              className="signin--form_username"
-              placeholder="Username"
-              required
-            />
-            <br />
-            <Field
-              name="password"
-              component="input"
-              className="signin--form_password"
-              placeholder="Password"
-              required
-            />
-            <br />
-            <button className="signin--form_button" action="submit" value="Sign In">
-              Sign In
-            </button>
-          </form>
-          <p className="signin--form_options"> Or sign In with </p>
-          {/* <div className="signin--buttons">
-            <button className="signin--buttons__facebook">
-              <i className="fab fa-facebook-square" />facebook
-            </button>
-            <button className="signin--buttons__google">
-              <img
-                src={logo}
-                alt="google logo"
-                className="signin--buttons__google--logo"
-              />Google
-            </button>
-          </div> */}
-          <p className="signin--form_notmember">
-            Not a member? <Link to="/signup"> Sign up </Link>
-          </p>
+        </p> */}
+          <img style={{maxHeight:'100%', width:'50%'}}src={signin} alt="img"/>
+          <div className="signin--box">
+            <h1 className="signin--box--title">
+              payMe
+              <span className="signin--box--dot">
+              .
+              </span>
+              <br />
+            </h1>
+            <h1 className="signin--header">Sign In</h1>
+            <h3>{this.props.message}</h3>
+            <form className="signin--form" onSubmit={handleSubmit(this.handleFormSubmit)}>
+              <Field
+                name="username"
+                component="input"
+                className="signin--form_username"
+                placeholder="Username"
+                required
+              />
+              <br />
+              <Field
+                name="password"
+                component="input"
+                className="signin--form_password"
+                placeholder="Password"
+                required
+              />
+              <br />
+              <button className="signin--form_button" action="submit" value="Sign In">
+                Sign In
+              </button>
+            </form>
+            <p className="signin--form_options"> Or sign In with </p>
+            {/* <div className="signin--buttons">
+              <button className="signin--buttons__facebook">
+                <i className="fab fa-facebook-square" />facebook
+              </button>
+              <button className="signin--buttons__google">
+                <img
+                  src={logo}
+                  alt="google logo"
+                  className="signin--buttons__google--logo"
+                />Google
+              </button>
+            </div> */}
+            <p className="signin--form_notmember">
+              Not a member? <Link to="/signup"> Sign up </Link>
+            </p>
+          </div>
         </div>
       </div>
     );
