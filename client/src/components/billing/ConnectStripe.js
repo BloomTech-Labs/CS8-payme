9ab56d254b2174208ef5097421172568d7b19923
AJ Genung <DragonRydrz@me.com>
2018-07-30 16:23:55 -0400

    ConnectStrip link updated

--- client/src/components/billing/ConnectStripe.js
+++ client/src/components/billing/ConnectStripe.js
@@ -4,9 +4,7 @@ export default class ConnectStripe extends Component {
   render() {
     return (
       <div>
-        <a
-          href={`${process.env.HOST}/stripe/authorize?jwt=${localStorage.getItem('id')}`}
-        >
+        <a href={`/stripe/authorize?jwt=${localStorage.getItem('id')}`}>
           This Component is for the user to connect their stripe account to their payMe account
         </a>
       </div>
