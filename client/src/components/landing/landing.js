cd7068a1727c0757c1181016db5544694696e80e
Maximo DeLaRosa <mjd809@gmail.com>
2018-08-08 10:08:34 -0400

    Fixed search bar media query

--- client/src/components/landing/landing.js
+++ client/src/components/landing/landing.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import landPic from './landing.jpg';
 import { autoLogin } from '../../actions/auth';
+import givme from './giveMe.png';
 
 const styles = {
   backgroundImage: `url(${landPic})`,
   backgroundSize: 'cover',
-  // backgroundPosition: 'center',
 };
 class Landing extends Component {
   componentDidMount() {
@@ -22,8 +22,9 @@ class Landing extends Component {
       <div className="landing">
         <div className="landing-container">
           <img src={landPic} style={styles} alt="SideLogo" className="landing-image" />
-          <p className="landing-title">payMe<span style={{color: '#22CFB1'}}>.</span></p>
-          <h1 className="landing-slogan">Lorem Ipsum</h1>
+          {/* <img src={givme} className="landing-logo" alt="logo" /> */}
+          <p className="landing-title">giveMe<span style={{ color: '#22CFB1' }}>.</span></p>
+          <h1 className="landing-slogan">myMoney</h1>
           <Link to="/signin">
             <button className="landing-signin">Sign In</button>
           </Link>
