4e187056d6c45957377ca64e2bb357feb7f47187
AJ Genung <DragonRydrz@me.com>
2018-08-09 11:29:57 -0400

    added recovery to userModel

--- server/models/users.js
+++ server/models/users.js
@@ -50,6 +50,10 @@ const User = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    recovery: {
+      code: Number,
+      exp: Number,
+    },
     invoices: [{ type: ObjectId, ref: 'Invoice' }],
   },
   {
