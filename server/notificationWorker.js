b6dd80748f6dfcc80273843aa2449f0c85a260ec
onHawk <luisd824@gmail.com>
2018-09-05 03:08:02 -0400

    include this file incase. not needed

--- server/notificationWorker.js
+++ server/notificationWorker.js
@@ -1,38 +1,10 @@
 const Reminder = require('./models/Reminder.js');
 
-const minuteWorker = function() {
-  return {
-    run: function() {
-      Reminder.Minute();
-    }
-  };
-};
-
-const dailyWorker = function() {
-  return {
-    run: function() {
-      Reminder.Daily();
-    }
-  };
-};
-const weeklyWorker = function() {
-  return {
-    run: function() {
-      Reminder.Weekly();
-    }
-  };
-};
-const monthlyWorker = function() {
-  return {
-    run: function() {
-      Reminder.Monthly();
-    }
-  };
-};
-
-module.exports = {
-  minuteWorker: minuteWorker(),
-  dailyWorker: dailyWorker(),
-  weeklyWorker: weeklyWorker(),
-  monthlyWorker: monthlyWorker()
-};
+// let n = reminder.frequency;
+// let dayRange = [];
+// while (n != 0) {
+//   startDate += 1;
+//   n--;
+//   dayRange.push(startDate);
+// }
+// return dayRange;
