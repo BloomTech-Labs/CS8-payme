bc08c7bde215523c65b1863431ea075ee3737aca
onHawk <luisd824@gmail.com>
2018-09-20 22:35:33 -0400

    display dates for each reminder

--- client/src/components/reminders/data/emailData.js
+++ client/src/components/reminders/data/emailData.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 import { deleteSms } from '../../../actions/smsReminders';
 
@@ -11,12 +12,24 @@ const Emails = props => {
     <div>
       Remind by Email:
       {emailReminders.map((r, i) => {
+        let start = moment(r.start);
+        start = start.format('MM ddd hh:mm');
+        let end = moment(r.end);
+        end = end.format('MM ddd hh:mm');
         return (
           <div style={{ border: '1px solid blue' }} key={i}>
             <span>
               <i className="fas fa-envelope-open fa-fw" />
             </span>
-            <div>{r.name}</div>
+            <div>{r.title}</div>
+            {r.remind !== 'custom' ? (
+              <p>
+                {start} - {end}
+              </p>
+            ) : (
+              <p>{start}</p>
+            )}
+            <p>{r.remind}</p>
             <span onClick={() => props.deleteSms({ reminderId: r._id, invoiceId: r.invoiceId })}>
               <i className="fa fa-bell-slash" />
             </span>
--- client/src/components/reminders/data/smsData.js
+++ client/src/components/reminders/data/smsData.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import moment from 'moment';
 import { deleteSms } from '../../../actions/smsReminders';
 
 const SMS = props => {
@@ -11,13 +12,25 @@ const SMS = props => {
     <div>
       Remind by sms:
       {smsReminders.map((r, i) => {
+        let start = moment(r.start);
+        start = start.format('MM ddd hh:mm');
+        let end = moment(r.end);
+        end = end.format('MM ddd hh:mm');
+        // console.log(start);
         return (
-          <div style={{ border: '1px solid red' }} key={i}>
+          <div style={{ display: 'flex', justifyContent: 'space-around' }} key={i}>
             <span>
               <i className="fas fa-mobile-alt" />
             </span>
-            <p>{r.name}</p>
-            <p>{r.date}</p>
+            <p>{r.title}</p>
+            {r.remind !== 'custom' ? (
+              <p>
+                {start} - {end}
+              </p>
+            ) : (
+              <p>{start}</p>
+            )}
+            <p>{r.remind}</p>
             <span onClick={() => props.deleteSms({ reminderId: r._id, invoiceId: r.invoiceId })}>
               <i className="fa fa-bell-slash" />
             </span>
